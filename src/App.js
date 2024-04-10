cb8a599de3371af2f12c4c353bffe7403c51d65c
yash <yashd6322@gmail.com>
2024-04-10 17:36:32 +0530

    yash-testing

--- src/App.js
+++ src/App.js
@@ -34,66 +34,69 @@ const App = () => {
   };
 
   return (
+    // <>
+    //   <Navbar />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //     {!isAuthenticated && (
+    //       <Route path="login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+    //     )}
+    //     {!isAuthenticated && (
+    //       <Route path="signup" element={<SignUp setIsAuthenticated={setIsAuthenticated} />} />
+    //     )}
+    //     {isAuthenticated && <Route path="Login" element={<Navigate to="/" />} />}
+    //     {isAuthenticated && <Route path="signup" element={<Navigate to="/" />} />}
+    //   </Routes>
+
+    //   <div style={{ textAlign: 'center' }}>
+    //     <h1 style={{ margin: '20px 0' }}>Category Details</h1>
+    //   </div>
+
+    //   <div>
+    //     <div style={{ textAlign: 'center' }}>
+    //       <button style={selectedCategory === 'Laptops' ? { ...buttonStyle, backgroundColor: 'Black' } : buttonStyle} onClick={() => handleCategorySelect('Laptops')}>Laptop</button>
+    //       <button style={buttonStyle} onClick={() => handleCategorySelect('Smartphones')}>Smartphones</button>
+    //       <button style={buttonStyle} onClick={() => handleCategorySelect('bottle')}>Bottles</button>
+    //       <button style={buttonStyle} onClick={() => handleCategorySelect('cars')}>Cars</button>
+    //     </div>
+    //     {categoryDetails.map((details, index) => (
+    //       <div key={index}>
+    //         {details && details.data && details.data.category && details.data.category.length > 0 && (
+    //           <>
+    //             {details.data.category.map((category, i) => (
+    //               selectedCategory === category.name && (
+    //                 <div key={i} style={{ textAlign: 'center' }}>
+    //                   <h1 style={{ margin: '18px 0' }}>{category.name}</h1>
+    //                   <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+    //                     {category.product_details.map((product, productIndex) => (
+    //                       <div key={productIndex} style={{ width: '350px', height: '250px', padding: '2px', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '38px' }}>
+    //                         {product.product_images.length > 0 && (
+    //                           <img
+    //                             src={product.product_images[0].image}
+    //                             alt={product.img}
+    //                             style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+    //                           />
+    //                         )}
+    //                         <h4 style={{ fontWeight: 'bold',margin: '10px 0' }}>{product.name}</h4>
+    //                       </div>
+    //                     ))}
+    //                   </div>
+    //                 </div>
+    //               )
+    //             ))}
+    //           </>
+    //         )}
+    //       </div>
+    //     ))}
+    //   </div>
+
+    //   <Testimonial />
+    //   <Footer />
+    // </>
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        {!isAuthenticated && (
-          <Route path="login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-        )}
-        {!isAuthenticated && (
-          <Route path="signup" element={<SignUp setIsAuthenticated={setIsAuthenticated} />} />
-        )}
-        {isAuthenticated && <Route path="Login" element={<Navigate to="/" />} />}
-        {isAuthenticated && <Route path="signup" element={<Navigate to="/" />} />}
-      </Routes>
-
-      <div style={{ textAlign: 'center' }}>
-        <h1 style={{ margin: '20px 0' }}>Category Details</h1>
-      </div>
-
-      <div>
-        <div style={{ textAlign: 'center' }}>
-          <button style={selectedCategory === 'Laptops' ? { ...buttonStyle, backgroundColor: 'Black' } : buttonStyle} onClick={() => handleCategorySelect('Laptops')}>Laptop</button>
-          <button style={buttonStyle} onClick={() => handleCategorySelect('Smartphones')}>Smartphones</button>
-          <button style={buttonStyle} onClick={() => handleCategorySelect('bottle')}>Bottles</button>
-          <button style={buttonStyle} onClick={() => handleCategorySelect('cars')}>Cars</button>
-        </div>
-        {categoryDetails.map((details, index) => (
-          <div key={index}>
-            {details && details.data && details.data.category && details.data.category.length > 0 && (
-              <>
-                {details.data.category.map((category, i) => (
-                  selectedCategory === category.name && (
-                    <div key={i} style={{ textAlign: 'center' }}>
-                      <h1 style={{ margin: '18px 0' }}>{category.name}</h1>
-                      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-                        {category.product_details.map((product, productIndex) => (
-                          <div key={productIndex} style={{ width: '350px', height: '250px', padding: '2px', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '38px' }}>
-                            {product.product_images.length > 0 && (
-                              <img
-                                src={product.product_images[0].image}
-                                alt={product.img}
-                                style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-                              />
-                            )}
-                            <h4 style={{ fontWeight: 'bold',margin: '10px 0' }}>{product.name}</h4>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  )
-                ))}
-              </>
-            )}
-          </div>
-        ))}
-      </div>
-
-      <Testimonial />
-      <Footer />
+    <div>I am testing this</div>
     </>
   );
 };
